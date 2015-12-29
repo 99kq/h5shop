3a368f8646bf9e31e5acf6c4fda0108827cabc24
Vitrum <vitrum.cn@gmail.com>
2015-12-29 18:03:34 +0800

    简单的就可以显示了，并没有不能点击栏目的问题。

--- src/modules/product/product.js
+++ src/modules/product/product.js
@@ -1,191 +1,53 @@
 require('./product.less');
 
-var service = require('./service'),
-    appFunc = require('../utils/appFunc'),
-    template = require('./product.tpl.html'),
-    inputModule = require('../input/input');
+var appFunc = require('../utils/appFunc'),
+    template = require('./product.tpl.html');
 
-var product = {
+var productView = {
     init: function(){
-        this.getList();
-        this.bindEvent();
+        productView.bindEvents();
+        // console.log("bindEvents productView");
     },
-    getList: function(){
-        var that = this;
-
-        service.getList(function(tl){
-            that.renderList(tl);
-
-            H5Shop.hideIndicator();
-
-            //Unlock scroll loading status
-            var ptrContent = $$('#productView').find('.pull-to-refresh-content');
-            ptrContent.data('scrollLoading','unloading');
-        });
-    },
-    refreshList: function(){
-
-        service.refreshList(function(tl){
-            // Find newest msg id in ptrContent;
-
-            product.refreshItemTime();
-
-            var newestId = $$('#productView').find('.product-list .card'). eq(0).data('id');
-
-            setTimeout(function () {
-
-                $$('#productView .refresh-click').find('i').removeClass('ios7-reloading');
-
-                if(parseInt(newestId) === 48) {
-                    product.showLoadResult(i18n.index.nothing_loaded);
-                    H5Shop.pullToRefreshDone();
-                    return false;
-                }
-
-                var length = tl.length;
-
-                if(length >= 15){
-                    product.renderList(tl);
-                }else if(length > 0){
-                    product.renderList(tl, 'prepend');
-                }else{
-                    product.showLoadResult(i18n.index.nothing_loaded);
-                }
-
-                H5Shop.pullToRefreshDone();
-
-            },1500);
-
-        });
-    },
-    infiniteList: function(){
-        var $this = $$(this);
+    renderProduct: function(){
+        if($$('#productView .page-content')[0]) return;
 
         H5Shop.showIndicator();
-        service.infiniteList(function(tl){
-            var status = $this.data('scrollLoading');
-            if (status === 'loading') return;
 
-            $this.data('scrollLoading','loading');
-
-            var items = $this.find('.product-list .card');
-            var length = items.length;
-            var lastId = items.eq(length - 1).data('id');
-            if(parseInt(lastId) === 24){
-                H5Shop.detachInfiniteScroll($this);
-                H5Shop.hideIndicator();
-            }else{
-
-                setTimeout(function(){
-                    $this.data('scrollLoading','unloading');
-                    product.renderList(tl, 'append');
-
-                    H5Shop.hideIndicator();
-                },1500);
-            }
-        });
-    },
-    refreshListByClick: function(){
-        setTimeout(function(){
-            $$('#productView .refresh-click').find('i').addClass('ios7-reloading');
-        },350);
-
-        $$('#productView .pull-to-refresh-content').scrollTop(0,300);
-
-        H5Shop.pullToRefreshTrigger('#productView .pull-to-refresh-content');
-    },
-    showLoadResult: function(text){
-        setTimeout(function(){
-            $$('#productView .load-result').html(text).css('opacity','1').transition(1000);
-
-            setTimeout(function(){
-                $$('#productView .load-result').css('opacity','0').transition(1000);
-            },2100);
-        },400);
-    },
-    refreshItemTime:function(){
-        $$('#productView').find('.card .ks-facebook-date').each(function(){
-            var nowTime = appFunc.timeFormat($$(this).data('time'));
-            $$(this).html(nowTime);
-        });
-    },
-    photoBrowser: function(){
-
-        var url = $$(this).attr('src');
-
-        var myPhotoBrowser = H5Shop.photoBrowser({
-            photos: [url],
-            toolbar: false,
-            backLinkText: i18n.global.close
-        });
-
-        myPhotoBrowser.open();
-
-    },
-    renderList: function(tl, type){
         var renderData = {
-            list: tl,
-            finalText: function(){
-                return appFunc.matchUrl(this.text);
-            },
-            time: function(){
-                return appFunc.timeFormat(this.created_at);
-            }
+            avatarUrl: 'http://lorempixel.com/68/68/people/7/',
+            nickName: 'H5Shop',
+            points: '100'
         };
-        console.log(template);
-        console.log(renderData);
-        var output = appFunc.renderTpl(template, renderData);
-        if(type === 'prepend'){
-            $$('#productView').find('.product-list').prepend(output);
-        }else if(type === 'append') {
-            $$('#productView').find('.product-list').append(output);
-        }else {
-            $$('#productView').find('.product-list').html(output);
-        }
-    },
-    openItemPage: function(e){
-        if(e.target.nodeName === 'A' || e.target.nodeName === 'IMG'){
-            return false;
-        }
-        var itemId = $$(this).data('id');
-        productF7View.router.loadPage('page/tweet.html?id=' + itemId);
-    },
-    bindEvent: function(){
 
+        var output = appFunc.renderTpl(template, renderData);
+        $$('#productView .page[data-page="product"]').html(output);
+
+        H5Shop.hideIndicator();
+    },
+    logOut: function(){
+        H5Shop.confirm(i18n.product.confirm_logout,function(){
+            mainView.router.loadPage('page/login.html');
+            H5Shop.showTab('#ourView');
+        });
+    },
+    bindEvents: function(){
         var bindings = [{
             element: '#productView',
-            selector: '.pull-to-refresh-content',
-            event: 'refresh',
-            handler: this.refreshList
+            event: 'show',
+            handler: productView.renderProduct
         },{
             element: '#productView',
-            selector: '.pull-to-refresh-content',
-            event: 'infinite',
-            handler: this.infiniteList
-        },{
-            element: '#productView',
-            selector: '.refresh-click',
+            selector: '.logout-button',
             event: 'click',
-            handler: this.refreshListByClick
+            handler: productView.logOut
         },{
             element: '#productView',
-            selector: 'a.open-send-popup',
+            selector: '.update-button',
             event: 'click',
-            handler: inputModule.openSendPopup
-        },{
-            element: '#productView',
-            selector: '.product-list .ks-facebook-card',
-            event: 'click',
-            handler: this.openItemPage
-        },{
-            element: '#productView',
-            selector:'div.card-content .item-image>img',
-            event: 'click',
-            handler: this.photoBrowser
+            //handler: productView.checkVersion
         }];
-
         appFunc.bindEvents(bindings);
     }
 };
 
-module.exports = product;
+module.exports = productView;
